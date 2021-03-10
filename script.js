c6bf346de49f56d9999b181f3b09a35e728f8906
Alex Bilba <alex.shanless@gmail.com>
2021-03-09 20:31:15 -0800

    st

---
+++ script.js
@@ -0,0 +1,10 @@
+const  fill = document.querySelector('.fill')
+const emp
+
+function dragStart() {}
+
+function dragEnd() {}
+function dragOver() {}
+function dragEnter() {}
+function dragLeave() {}
+function dragDrop() {}
